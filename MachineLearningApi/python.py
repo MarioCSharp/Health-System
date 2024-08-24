b6741d10b7987ad9329f0a931d8253e10efe7162
Mario Petkov <mario.18@abv.bg>
2024-08-24 19:11:20 +0300

    ML model update.

--- MachineLearningApi/python.py
+++ MachineLearningApi/python.py
@@ -12,21 +12,32 @@ data = data.dropna()
 
 data['Симптоми'] = data['Симптоми'].str.split(', ')
 
+def subtract_symptoms(symptom_lists):
+    first = symptom_lists.iloc[0]
+    rest = set(sum(symptom_lists.iloc[1:], []))
+    return [symptom for symptom in first if symptom not in rest]
+
+data_grouped = data.groupby('Диагноза').agg({
+    'Симптоми': subtract_symptoms,
+    'Предотвратяване': 'first',
+    'Лекар': 'first'
+}).reset_index()
+
 mlb_symptoms = MultiLabelBinarizer()
-symptoms_encoded = mlb_symptoms.fit_transform(data['Симптоми'])
+symptoms_encoded = mlb_symptoms.fit_transform(data_grouped['Симптоми'])
 
 symptoms_df = pd.DataFrame(symptoms_encoded, columns=mlb_symptoms.classes_)
-data = pd.concat([data, symptoms_df], axis=1)
+data_grouped = pd.concat([data_grouped, symptoms_df], axis=1)
 
-data = data.drop('Симптоми', axis=1)
+data_grouped = data_grouped.drop('Симптоми', axis=1)
 
-data['Комбинирано'] = data['Диагноза'] + '|' + data['Предотвратяване'] + '|' + data['Лекар']
+data_grouped['Комбинирано'] = data_grouped['Диагноза'] + '|' + data_grouped['Предотвратяване'] + '|' + data_grouped['Лекар']
 
 le = LabelEncoder()
-data['Комбинирано'] = le.fit_transform(data['Комбинирано'])
+data_grouped['Комбинирано'] = le.fit_transform(data_grouped['Комбинирано'])
 
-X = data.drop(['Диагноза', 'Предотвратяване', 'Лекар', 'Комбинирано'], axis=1)
-y = data['Комбинирано']
+X = data_grouped.drop(['Диагноза', 'Предотвратяване', 'Лекар', 'Комбинирано'], axis=1)
+y = data_grouped['Комбинирано']
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
@@ -34,6 +45,7 @@ model = RandomForestClassifier(n_estimators=100, random_state=42)
 model.fit(X_train, y_train)
 
 y_pred = model.predict(X_test)
+
 print("Accuracy:", accuracy_score(y_test, y_pred))
 print(classification_report(y_test, y_pred))
 
