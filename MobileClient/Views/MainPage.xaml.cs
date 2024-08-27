1784d2df6d6036bfce4737699e185f8e7b877937
Mario Petkov <mario.18@abv.bg>
2024-08-27 16:30:47 +0300

    Added the buttons to the main page.

--- MobileClient/Views/MainPage.xaml.cs
+++ MobileClient/Views/MainPage.xaml.cs
@@ -43,5 +43,25 @@ namespace HealthProject.Views
             base.OnAppearing();
             this.mainPageViewModel.LoadResources();
         }
+
+        private async void OnMedicineButtonClicked(object sender, EventArgs e)
+        {
+            await Shell.Current.GoToAsync($"//{nameof(MedicationViewPage)}");
+        }
+
+        private async void OnRecordsButtonClicked(object sender, EventArgs e)
+        {
+            await Shell.Current.GoToAsync($"//{nameof(LogbookViewPage)}");
+        }
+
+        private async void OnDocumentsButtonClicked(object sender, EventArgs e)
+        {
+            await Shell.Current.GoToAsync($"//{nameof(DocumentViewPage)}");
+        }
+
+        private async void OnPredictorButtonClicked(object sender, EventArgs e)
+        {
+            await Shell.Current.GoToAsync($"//{nameof(DiagnosisPredictionPage)}");
+        }
     }
 }
